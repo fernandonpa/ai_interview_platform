80bdb4fa1b4fecf0d4564cd61355f461da3a2d8c
fernandonpa <158037288+fernandonpa@users.noreply.github.com>
2025-07-09 19:49:11 +0530

    setup interview card

---
+++ lib/actions/general.action.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { db } from "@/firebase/admin";
+
+export async function getFeedbackByInterviewId(
+  params: GetFeedbackByInterviewIdParams
+): Promise<Feedback | null> {
+  const { interviewId, userId } = params;
+
+  const querySnapshot = await db
+    .collection("feedback")
+    .where("interviewId", "==", interviewId)
+    .where("userId", "==", userId)
+    .limit(1)
+    .get();
+
+  if (querySnapshot.empty) return null;
+
+  const feedbackDoc = querySnapshot.docs[0];
+  return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
+}
