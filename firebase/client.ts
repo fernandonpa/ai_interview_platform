55a413cdabc2c91df00db788b476b560e24db742
fernandonpa <158037288+fernandonpa@users.noreply.github.com>
2025-07-09 18:38:42 +0530

    add firebase client

---
+++ firebase/client.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAsjkbv-aGYeqgL3o07XtPQF6cbfRpqXdI",
+  authDomain: "prepwise-56d92.firebaseapp.com",
+  projectId: "prepwise-56d92",
+  storageBucket: "prepwise-56d92.firebasestorage.app",
+  messagingSenderId: "753276523469",
+  appId: "1:753276523469:web:85c4c9d74304125946aa84",
+  measurementId: "G-FCEQHL47FZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
