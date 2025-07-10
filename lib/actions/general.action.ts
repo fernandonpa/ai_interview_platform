51d42701bb99a794426da950cbd05137567f7ea2
fernandonpa <158037288+fernandonpa@users.noreply.github.com>
2025-07-10 11:06:40 +0530

    add interview page

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import { db } from "@/firebase/admin";
 import { generateObject } from "ai";
 import { google } from "@ai-sdk/google";
 
+import { db } from "@/firebase/admin";
 import { feedbackSchema } from "@/constants";
 
 export async function createFeedback(params: CreateFeedbackParams) {
@@ -23,17 +23,17 @@ export async function createFeedback(params: CreateFeedbackParams) {
       }),
       schema: feedbackSchema,
       prompt: `
-          You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
-          Transcript:
-          ${formattedTranscript}
-  
-          Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
-          - **Communication Skills**: Clarity, articulation, structured responses.
-          - **Technical Knowledge**: Understanding of key concepts for the role.
-          - **Problem-Solving**: Ability to analyze problems and propose solutions.
-          - **Cultural & Role Fit**: Alignment with company values and job role.
-          - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
-          `,
+        You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+        Transcript:
+        ${formattedTranscript}
+
+        Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+        - **Communication Skills**: Clarity, articulation, structured responses.
+        - **Technical Knowledge**: Understanding of key concepts for the role.
+        - **Problem-Solving**: Ability to analyze problems and propose solutions.
+        - **Cultural & Role Fit**: Alignment with company values and job role.
+        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+        `,
       system:
         "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
     });
@@ -64,6 +64,12 @@ export async function createFeedback(params: CreateFeedbackParams) {
     console.error("Error saving feedback:", error);
     return { success: false };
   }
+}
+
+export async function getInterviewById(id: string): Promise<Interview | null> {
+  const interview = await db.collection("interviews").doc(id).get();
+
+  return interview.data() as Interview | null;
 }
 
 export async function getFeedbackByInterviewId(
