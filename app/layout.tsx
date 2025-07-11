ab11bc67cf99c4716b96f1dd013344be9c198fbd
fernandonpa <158037288+fernandonpa@users.noreply.github.com>
2025-07-11 08:25:08 +0530

    modify the auth layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
@@ -8,7 +9,7 @@ const monaSans = Mona_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "PrepWise-AI",
+  title: "CareerPilot",
   description: "AI-Powered Interview Preparation Platform",
 };
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${monaSans.className}  antialiased pattern`}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
